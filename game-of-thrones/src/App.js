5110db85af04ccd4f9a8e26626193db0094dc925
dev-augustin <dev.augustin00@gmail.com>
2020-04-01 11:43:07 -0500

    added basic code but access blocked by CORS policy error

--- game-of-thrones/src/App.js
+++ game-of-thrones/src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    //|||||||  Fetch   ||||||||||
+    // fetch("https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=27a02bbb5b48401f96bfda6a7d3e2545")
+    //   .then(response => response.json()) // first response => resolved
+    //   .then(
+    //     result => {
+    //       console.log(result.results)
+
+    //       this.setState({
+    //         isLoaded: true,
+    //         items:result.results
+    //       })
+
+    //       console.log("state is updated",this.state.data)
+    //     }
+    //   ).catch(e => console.log("there's a error", e))
+    // ||||||||||||||||||||
+    Axios.get("http://anapioficeandfire.com/api/characters/583").then(res => {
+      const born = res.data.results;
+      console.log("recipes ---->", born)
+      this.setState({data:born})
+    })
+    .catch(error => {
+      console.log('there is an error', error)
+    })
+  
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Spoonacular API</h1>
+        <ul>
+        {this.state.data.map((born, id) => <li key={id}> {born.born} </li>)}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+        // {this.state.data.map((recipe, id) => <li key={id}> {recipe.title} </li>)}
+ 
+ 
+
+
+
+
+  
+  
+
